036e1a920550ac459dcde3d9ce229b5eb377a658
thiencutovc <thien.lm204608@sis.hust.edu.vn>
2022-11-23 20:08:19 +0700

    template engine - handlerbars

---
+++ blog/src/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const { engine  } = require('express-handlebars')
+const port = 3000
+const morgan = require('morgan')
+
+//http logger
+app.use(morgan('combined'))
+
+//template engine
+app.engine('hbs', engine({
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resource/views'))
+
+ 
+app.get("/", (req, res) => {
+    res.render('home')
+})
+
+app.get("/news", (req, res) => {
+    res.render('news')
+})
+
+
+app.listen(port, () => console.log('example app')) 
