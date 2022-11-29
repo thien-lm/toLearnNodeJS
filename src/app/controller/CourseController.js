ec4c0417669101a7ac4f7c3c9cb553eba2d6b4b0
thiencutovc <thien.lm204608@sis.hust.edu.vn>
2022-11-29 19:51:45 +0700

    switch branch

--- src/app/controller/CourseController.js
+++ src/app/controller/CourseController.js
@@ -42,6 +42,12 @@ class CourseController {
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next)
     }
+    //put [delete]
+    delete(req, res, next) {
+        Course.deleteOne({_id : req.params.id})
+            .then(() => res.redirect('back'))
+            .catch(next)
+    }
 }
 
 module.exports = new CourseController();
--- src/routes/course.js
+++ src/routes/course.js
@@ -6,6 +6,7 @@ const courseController = require('../app/controller/CourseController');
 router.get('/create', courseController.create);
 
 router.get('/:id/edit', courseController.edit)
+router.delete('/:id', courseController.delete);
 router.put('/:id', courseController.update);
 router.post('/store', courseController.store)
 router.get('/:slug', courseController.show);
