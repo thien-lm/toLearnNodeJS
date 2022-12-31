f1809785c68c510ab2f020e60bccca7abb2347f1
thiencutovc <thien.lm204608@sis.hust.edu.vn>
2022-12-31 22:44:09 +0700

    add ajax component to send comment withou reloading page

--- src/app/controller/CourseController.js
+++ src/app/controller/CourseController.js
@@ -3,37 +3,63 @@ const Comment = require('../models/Comment')
 const { toObject, multipleToObject } = require('../../util/convertToObject');
 class CourseController {
 
+    // comment(req, res, next) {
+    //     // req.body.name = req.user.name;
+    //     req.body.name = req.user.name
+    //     req.body.email = req.user.email
+    //     req.body.video = req.body.videoID
+    //     console.log(req.body)
+        
+    //     const comment = new Comment(req.body);
+    //     comment
+    //         .save()
+    //         .then(() => res.redirect('back'))
+    //         .catch('404 ERROR');
+    // }
     comment(req, res, next) {
-        console.log('to mcomment')
-        // req.body.name = req.user.name;
+        console.log('cac')
         req.body.name = req.user.name
         req.body.email = req.user.email
-        console.log(req.body)
-        
+        req.body.video = req.body.videoID
+        // console.log(req.body)
         const comment = new Comment(req.body);
-        comment
-            .save()
-            .then(() => res.redirect('back'))
-            .catch('404 ERROR');
-        console.log('cac')
+        // Comment.findOne({video: req.body.videoID}).then(next => {
+        //     console.log(next)
+        // })
+        comment.save( {
+            $push: {
+                message: {name: req.user.name, message: req.body.message}
+            }
+        },
+        (error, post) => {
+            Comment.find({video:  req.body.videoID}).then(comment => res.send(multipleToObject(comment)))
+        }
+            
+            )
     }
-
     //[GET] news
 
     show(req, res, next) {
+
         Course.findOne({ slug: req.params.slug })
-            .then((course) => {
+          .then((course) => {
+            Comment.find({video: course.videoId})
+              .then((comment) => {    
                 Course.find({}).
                     then((courses) => {
                         let randomNumber = Math.floor(Math.random()*3)
-                                res.render('courses/show', { course: toObject(course) ,
-                                                 courses: multipleToObject(courses).slice(randomNumber, randomNumber + 3 )}
+                                res.render('courses/show', { 
+                                                course: toObject(course) ,
+                                                courses: multipleToObject(courses).slice(randomNumber, randomNumber + 3 ),
+                                                comment: multipleToObject(comment)}
                                              );
                 })
-
             })
+            }
+            )
             .catch(next);
     }
+
 
     create(req, res, next) {
         res.render('courses/create');
