d39ab84dc8f8f9b614ccaed55ac6661217d654d5
thiencutovc <thien.lm204608@sis.hust.edu.vn>
2023-01-01 16:35:39 +0700

    abcxyz

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const { engine } = require('express-handlebars');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const morgan = require('morgan');
 
 const routes = require('./routes');
